2cf822e929537cf3c6d4130d5918361c8b1975bc
chakii19 <iyanryan939@gmail.com>
2023-07-24 09:57:09 -0700

    menu.blade.php

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -4,4 +4,17 @@
       <div>
         <ul class="navbar-nav">
             <li class="nav-item">
-                
+                <a class="nav-link" href="/home">Home</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="/buku">Data Buku</a>
+            </li>
+
+            
+            <li class="nav-item">
+                <a class="nav-link" href="/anggota">Data Anggota</a>
+            </li>
+
+            
+            
