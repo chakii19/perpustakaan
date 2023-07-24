6ac9339b917de1ab125bc5d4cc28f2d5c6881b5a
chakii19 <iyanryan939@gmail.com>
2023-07-24 10:04:21 -0700

    banner.blade.php

--- resources/views/banner.blade.php
+++ resources/views/banner.blade.php
@@ -1,3 +1,3 @@
 <div class="col-md-12">
-    <img src="{{ asset ('gambar') }}/bg.jpeg" width="100%" height="280px">
+    <img src="{{ asset ('gambar') }}/perpustakaan.jpeg" width="100%" height="280px">
 </div>
