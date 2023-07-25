5d733ec06a0694577eaba45faa0e6b0599cc55f9
chakii19 <iyanryan939@gmail.com>
2023-07-25 10:52:09 -0700

    BukuController.php

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -4,7 +4,44 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\BukuModel;
+
 class BukuController extends Controller
+
 {
-    //
+    public function bukutampil()
+    {
+        $databuku = BukuModel::orderby('kode_buku', 'ASC')
+        ->paginate(5);
+
+        return view('halaman/view_buku' ,['buku' =>$databuku]);
+    }
+    public function bukutambahan(Request $request)
+    {
+        $this->validate($request, [
+            'kode_buku' =>'required',
+            'judul' => 'required',
+            'pengarang' =>'required',
+            'kategori' =>'required'
+        ]);
+
+        BukuModel::create([
+            'kode_buku' => $request->kode_buku,
+            'judul' => $request->judul,
+            'pengarang' =>$request->pengarang,
+            'kategori' =>$request->kategori
+        ]);
+        return redirect('/buku');
+    }
+    public function bukuhapus($id_buku)
+    {
+        $databuku=BukuModel::find($id_buku);
+        $databuku->delete();
+
+        return redirect()->back();
+    }
+    public function bukuedit($id_buku, Request $request)
+    {
+        
+    }
 }
