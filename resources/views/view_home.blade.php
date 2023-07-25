e2d18f0bc40f6d4adc776a287d884c92b5bf8e73
chakii19 <iyanryan939@gmail.com>
2023-07-25 10:34:44 -0700

    footer.blade.php

---
+++ resources/views/view_home.blade.php
@@ -0,0 +1,9 @@
+@extends('index')
+@section('title','home')
+@section('isihalaman')
+    <h3>Pengertian perpustakaan menurut para ahli</h3>
+    <p>
+    <h4>Perpustakaan</h4>
+    perpustakaan perguruan tinggi merupakan unit kerja pelaksana teknis (UPT) perguruan tinggi yang bersama-sama dengan unit lain turut melaksanakan tri dharma perguruan tinggi yaitu
+    
+@endsection
