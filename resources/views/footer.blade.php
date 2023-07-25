c08a6bd81dfb18701b509e2fd1e601f38e7014b8
chakii19 <iyanryan939@gmail.com>
2023-07-25 09:51:08 -0700

    footer.blade.php

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,13 @@
+<p>
+
+    <div class="col-md-12">
+        <div class="row" style="background:rgba(63, 67, 102, 0.562)">
+            <div class="col-md-9">
+                <h3>Visi</h3>
+                <p>Mengembangkan Budaya Membaca</p>
+
+                <p>
+                    <h3>Misi</h3>
+                    <ul>
+                        <li></li>
+                    </ul>
