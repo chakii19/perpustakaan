6604a10857f369b90fa645c5f6b0f260d62abea5
chakii19 <iyanryan939@gmail.com>
2023-07-25 11:06:51 -0700

    BukuModel.php

--- app/Models/BukuModel.php
+++ app/Models/BukuModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class BukuModel extends Model
+class  extends Model
 {
     use HasFactory;
 }
